ef527e6b2a2a203290d3c1a92b99cf52134d1d69
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-02-18 22:43:24 +0200

    Implementation of ray background from React bits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono} from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -27,7 +28,29 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${ martianMono.variable}  min-h-screen antialiased`}
       >
-        {children}
+        <div className={'absolute inset-0 top-0 z-[-1] m-h-screen'}>
+
+
+          <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.5}
+              rayLength={3}
+              followMouse={true}
+              mouseInfluence={0.02}
+              noiseAmount={0}
+              distortion={0.01}
+              className="custom-rays"
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}
+          />
+        </div>
+        <main>
+            {children}
+        </main>
+
       </body>
     </html>
   );
