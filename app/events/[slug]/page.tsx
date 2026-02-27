013d2d66a4fd80e077708160cd1b4423797c2fdf
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-02-27 13:54:56 +0200

    Implementation of Event booking component

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {notFound} from "next/navigation";
 import Image from 'next/image'
+import BookEvent from "@/components/BookEvent";
 
 const EventDetailsItem = ({icon,alt,label}:{icon:string,alt:string,label:string}) =>(
     <div className={'flex gap-2 items-center'}>
@@ -10,11 +11,11 @@ const EventDetailsItem = ({icon,alt,label}:{icon:string,alt:string,label:string}
 )
 
 const EventAgendaItem = ({agendaItems}:{agendaItems:string[]}) => (
-    <div className={'agenda'}>
+    <div className={'flex flex-col gap-2'}>
         <h2>Agenda</h2>
         <ul>
             {agendaItems.map((item) => (
-                <li key={item}>{item}</li>
+                <li className={'text-light-100 text-lg max-sm:text-sm'} key={item}>{item}</li>
             ))}
         </ul>
     </div>
@@ -38,17 +39,19 @@ const EventDetailsPage = async ({params}:{params:Promise<{slug:string}>}) => {
     const {sanitisedEvent:{organizer,description,image,overview,title,date,time,location,mode,agenda,audience,tags}} = await request.json();
 
     if(!description) return notFound()
+
+    const bookings = 10;
     return (
         <section id='id'>
-            <div className={'header'}>
+            <div className={'flex w-2/3 flex-col items-start gap-4 max-lg:w-full mb-10'}>
                 <h1>Event Description</h1>
                 <p>{description}</p>
             </div>
 
-            <div className={'details'}>
+            <div className={'flex w-full flex-col lg:flex-row gap-12 items-start mt-12 max-lg:items-center'}>
                 {/*Left side*/}
 
-                <div className={'content'}>
+                <div className={'flex flex-[2] flex-col gap-8 max-lg:w-full'}>
                     <Image src={image} alt={title} width={810} height={800} className={'banner'}/>
 
                     <section className={'flex-col-gap-2'}>
@@ -76,8 +79,19 @@ const EventDetailsPage = async ({params}:{params:Promise<{slug:string}>}) => {
                 </div>
 
                 {/*Right side*/}
-                <aside className={'booking'}>
-                    <p className={'text-lg font-semibold'}>Book Event</p>
+                <aside className={'flex-1 w-full p-4 border-l border-gray-700'}>
+                    <div className={'bg-dark-100 border-dark-200 card-shadow flex w-full flex-col gap-6 rounded-[10px] border px-5 py-6'}>
+                        <h2>Book Your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className={'text-sm'}>
+                                Join {bookings} people interested in this event.
+                            </p>
+                        ):(
+                            <p className={'text-sm'}>Be the first to book your spot!</p>
+                        )}
+
+                        <BookEvent/>
+                    </div>
                 </aside>
             </div>
         </section>
